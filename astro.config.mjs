40e86bcaabee33deb87824d35e31866f3d2f6a74
tsa96 <tom@trumpetdust.org>
2025-07-17 04:56:26 +0100

    Vite warning

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    build: {
+      // Stop Vite yelling about chunk with dashjs inside
+      chunkSizeWarningLimit: 1000
+    }
+  }
+});
